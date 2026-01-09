9e9b832b90afd4d0858cec08b04f3e27a4e82df1
Al Riyan Khan <alriyankhan7678@gmail.com>
2026-01-09 21:10:25 +0530

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import cv2
+from mss import mss
 import numpy as np
 import pyautogui
-from mss import mss
+
+
+def capture_screen_region_opencv(x,y,desired_width,desired_height):
+    ss = pyautogui.screenshot(region=(x,y,desired_width,desired_height))
+    ss = np.array(ss)
+    ss = cv2.cvtColor(ss,cv2.COLOR_RGB2BGR)
+    return ss
